6fd10a9aa38e40c52928bd51d28b4d3cd2f492f8
hurui <hooray2010@foxmail.com>
2018-01-29 16:47:22 +0800

    update data source config

---
+++ webapps/order-site/src/test/java/com/hr/cloud/controller/TestControllerTest.java
@@ -0,0 +1,18 @@
+package com.hr.cloud.controller;
+
+import com.hr.cloud.OrderSiteApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by hurui on 2018/1/29.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@Import(OrderSiteApplication.class)
+public class TestControllerTest {
+
+
+}
