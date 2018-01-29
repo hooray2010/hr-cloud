866ec8af94ba8d33e36b8b7fc86900e9f7b6387b
hurui <hooray2010@foxmail.com>
2018-01-29 11:27:08 +0800

    update redisConfig

--- webapps/order-site/src/main/java/com/hr/cloud/config/RedisConfig.java
+++ webapps/order-site/src/main/java/com/hr/cloud/config/RedisConfig.java
@@ -13,31 +13,36 @@ import org.springframework.data.redis.serializer.*;
 public class RedisConfig {
   
   //string
-  private static final RedisSerializer stringRedisSerializer = new StringRedisSerializer();
+  private static final RedisSerializer stringSerializer = new StringRedisSerializer();
   
   //object --> string
-  private static final RedisSerializer longRedisSerializer = new GenericToStringSerializer(Long.TYPE);
+  private static final RedisSerializer longSerializer = new GenericToStringSerializer(Long.TYPE);
   
   //object --> json, 无须类型参数
-  private static final RedisSerializer genericJsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+  private static final RedisSerializer genericJsonSerializer = new GenericJackson2JsonRedisSerializer();
   //type --> json, 须类型参数
-  private static final RedisSerializer jsonRedisSerializer = new Jackson2JsonRedisSerializer(Long.TYPE);
+  private static final RedisSerializer jsonSerializer = new Jackson2JsonRedisSerializer(Long.TYPE);
   
   //速度快, 但占用空间大
   //使用JDK的序列化手段(须实现serializable接口，ObjectInputStream，ObjectOutputStream)，数据以字节流存储
-  private static final RedisSerializer jdkRedisSerializer = new JdkSerializationRedisSerializer();
-  
-  @Bean
-  public RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory connectionFactory) {
-    final RedisTemplate<String, String> template = new RedisTemplate<>();
-    template.setConnectionFactory(connectionFactory);
-    return template;
-  }
+  private static final RedisSerializer jdkSerializer = new JdkSerializationRedisSerializer();
+
+//  //已存在实现类stringRedisTemplate, 这段代码不会执行!
+//  @Bean
+//  public RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+//    final RedisTemplate<String, String> template = new RedisTemplate<>();
+//    template.setConnectionFactory(connectionFactory);
+//    template.setKeySerializer(stringSerializer);
+//    template.setValueSerializer(stringSerializer);
+//    return template;
+//  }
   
   @Bean
   public RedisTemplate<String, Long> longRedisTemplate(RedisConnectionFactory connectionFactory) {
     final RedisTemplate<String, Long> template = new RedisTemplate<>();
     template.setConnectionFactory(connectionFactory);
+    template.setKeySerializer(stringSerializer);
+    template.setValueSerializer(longSerializer);
     return template;
   }
   
@@ -45,10 +50,10 @@ public class RedisConfig {
   public RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory connectionFactory) {
     final RedisTemplate<String, Object> template = new RedisTemplate<>();
     template.setConnectionFactory(connectionFactory);
-    RedisSerializer redisSerializer = new GenericJackson2JsonRedisSerializer();
-    template.setKeySerializer(redisSerializer);
-    template.setHashKeySerializer(redisSerializer);
-    template.setValueSerializer(redisSerializer);
+    template.setKeySerializer(stringSerializer);
+    template.setValueSerializer(genericJsonSerializer);
+    template.setHashKeySerializer(stringSerializer);
+    template.setHashValueSerializer(genericJsonSerializer);
     return template;
   }
   
